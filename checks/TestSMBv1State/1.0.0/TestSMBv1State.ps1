dc496062ba1d3bf47b5dc8d7538a5be91233cd81
Olaf Reitz <59179928+Adverus@users.noreply.github.com>
2026-09-07 15:48:22 +0200

    Einbau des OCTO_RESULT Zeugs, Anpassung der ui.json

--- checks/TestSMBv1State/1.0.0/TestSMBv1State.ps1
+++ checks/TestSMBv1State/1.0.0/TestSMBv1State.ps1
@@ -5,3 +5,7 @@ if ($SMB1Protocol.State -ne 'Disabled' -and $SMB1Protocol.State -ne 'DisabledWit
 }
 $output.State = "SMBv1 is disabled"
 $output.checkResult = 0
+
+$json = $output | ConvertTo-Json -Depth 10 -Compress
+$bytes = [System.Text.Encoding]::UTF8.GetBytes($json)
+[Console]::Out.Write("<<<OCTO_RESULT_BEGIN>>>" + [System.Convert]::ToBase64String($bytes) + "<<<OCTO_RESULT_END>>>")
